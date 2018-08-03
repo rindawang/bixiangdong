a8aab0bdeb5e0ceb95df4eef6df918c0460e066e
rinda wang <rinda.wang@sap.com>
2018-08-03 11:04:00 +0800

    properties

---
+++ day1-14/ioStream/PropertiesDemo.java
@@ -0,0 +1,11 @@
+package ioStream;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class PropertiesDemo {
+	public static void main(String[] args){
+		Map p = new Properties();
+	}
+
+}
