9ccc54636c3085da8a20fb9b7a519d5568c2a217
rinda wang <rinda.wang@sap.com>
2018-08-06 19:44:55 +0800

    thread 02

--- day1-14/com/rinda/thread/ThreadDemo.java
+++ day1-14/com/rinda/thread/ThreadDemo.java
@@ -1,31 +1,38 @@
 package com.rinda.thread;
 
 public class ThreadDemo {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Demo d1 = new Demo("d1");
 		Demo d2 = new Demo("d2");
-	    d1.start();
-	    d2.start();
-	    System.out.println(4/0);
-		for(int i = 0; i < 10; i ++){
-			System.out.println("Thread:"+ Thread.currentThread().getName());
+		Thread s1 = new Thread(d1);
+		Thread s2 = new Thread(d2);
+
+		s1.start();
+		s2.start();
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Thread:" + Thread.currentThread().getName());
 		}
 
 	}
 
 }
 
-class Demo extends Thread{
+class Demo implements Runnable {
 	private String name;
-	Demo(String name){
-		super(name);
+
+	Demo(String name) {
 		this.name = name;
 	}
-	
-	public void run(){
-		for(int i = 0; i < 10; i ++){
-			System.out.println(name + "....i=" + i + "Thread:"+ Thread.currentThread().getName());
+
+	public void show() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(name + "....i=" + i + "Thread:" + Thread.currentThread().getName());
 		}
+	}
+
+	public void run() {
+		show();
 	}
 
 }
